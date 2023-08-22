fcb0a30f06b3dbffabefee2cc8fe7c0661b975f8
hee-bin <gmlqls4732@naver.com>
2023-08-22 09:16:50 +0900

    eng_st로 주제 변경 후 기본  메인구성

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 
+const questions = [
+  "What is your favorite hobby?",
+  "Describe your last vacation.",
+  "Do you prefer reading books or watching movies?",
+  "What do you like to do on weekends?",
+  "Tell me about a memorable experience you've had."
+];
+
+const questionElement = document.getElementById("question");
+const nextButton = document.getElementById("nextButton");
+
+nextButton.addEventListener("click", showRandomQuestion);
+
+function showRandomQuestion() {
+  const randomIndex = Math.floor(Math.random() * questions.length);
+  const randomQuestion = questions[randomIndex];
+  questionElement.textContent = randomQuestion;
+}
+
+// Initial question display
+showRandomQuestion();
